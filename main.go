972b12a3d687c284958d96ea3053e8f5f32e4615
Eduardo Cano <jose.cano6053@alumnos.udg.mx>
2020-09-29 15:33:42 -0500

    prueba01

--- main.go
+++ main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var e float64
+	var nFactorial int
+	var limite int
+	e = 0
+	fmt.Scanln(&limite)
+
+	for n := 0; n < limite; n++ {
+		nFactorial = n
+		if n > 1 {
+			for i := n; i > 1; i-- {
+				nFactorial = nFactorial * (i - 1)
+			}
+			e = e + (1 / float64(nFactorial))
+		} else {
+			e = e + (1 / 1)
+		}
+	}
+	fmt.Println(e)
 }
